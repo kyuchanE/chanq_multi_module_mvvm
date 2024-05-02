91a56a6a646c9c8ccd72ffad730aa37f8664576b
KiU <41310752+kyuchanE@users.noreply.github.com>
2024-05-02 09:43:46 +0900

    commit

--- workspace/app/build.gradle.kts
+++ workspace/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":core:data"))
+
     implementation(project(":core:domain"))
     implementation(project(":core:ui"))
     implementation(project(":feature:lottery"))
